4982f5d75504bd4eaf7008a953c1a8e746d794ac
Gregori Santos <gresan69@gmail.com>
2018-02-22 00:19:28 -0300

    Reserva.

--- Futeweb/src/br/com/futeweb/aplicacao/admin/bean/AdminBean.java
+++ Futeweb/src/br/com/futeweb/aplicacao/admin/bean/AdminBean.java
@@ -38,6 +38,10 @@ public class AdminBean extends AdminVO implements Serializable{
 		
 	}
 	
+	public void reservar(){
+		
+	}
+	
 	public void cadastrarEstabelecimento() {
 		if (estabelecimento.validarObjeto(estabelecimento)){
 			try {
